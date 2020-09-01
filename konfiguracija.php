23d8a6905476293949c8803498758758a1cd58d2
iivansic <ivan.ivansic@sdfgroup.com>
2020-09-01 20:40:03 +0200

    dodan if u podnozje rad lokalno/produkcija komentar

--- konfiguracija.php
+++ konfiguracija.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'ip' => $_SERVER['REMOTE_ADDR'] === '127.0.0.1' ? true : false,
     'dev' => true,
     'nazivAPP' => 'Edunova APP',
     'url'=> 'http://polaznik04.edunova.hr/'
