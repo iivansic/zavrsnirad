5d8fef272e4acd8478ad05d275f09c706e038227
iivansic <ivan.ivansic@sdfgroup.com>
2020-09-01 21:57:44 +0200

    prosljeđivanje parametara iz kontrolera u view

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -12,4 +12,35 @@ class IndexController extends Controller
     {
         $this->view->render('onama');
     }
+    public function login()
+    {
+        $this->view->render('login',[
+            'email' => '',
+            'poruka' => 'Popunite traženje podatke'
+        ]);
+    }
+    public function autorizacija()
+    {
+        if(!isset($_POST['email']) || !isset($_POST['lozinka'])){
+            $this->login();
+            return;
+        }
+        if(strlen(trim($_POST['email']))===0){
+            $this->view->render('login',[
+                'email' => '',
+                'poruka' => 'Obavezan unos email-a'
+            ]);
+            return;
+        }
+        if(strlen(trim($_POST['lozinka']))===0){
+            $this->view->render('login',[
+                'email' => trim($_POST['email']),
+                'poruka' => 'Obavezan unos lozinke'
+            ]);
+            return;
+        }
+        // 100 % siguran da imam email i lozinku
+       
+    }
+
 }
