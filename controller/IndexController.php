70e801d21b0d4e0f423789e3b6827b7cf5628d11
iivansic <ivan.ivansic@sdfgroup.com>
2020-09-01 20:49:37 +0200

    slanje podataka iz kontrolera na view

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -3,7 +3,10 @@ class IndexController extends Controller
 {
     public function index()
     {
-       $this->view->render('pocetna');
+       $this->view->render('pocetna',[
+           'kljuc1' => 'Vrijednost1',
+           'kljuc2' => [1,2,7,9]
+       ]);
     }
     public function onama()
     {
