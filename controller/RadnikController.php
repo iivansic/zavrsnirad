2ef37a1beef93f7977faaa67be192186c9821876
iivansic <ivan.ivansic@sdfgroup.com>
2020-09-13 00:33:17 +0200

    vizualno dodavanje CRUD

--- controller/RadnikController.php
+++ controller/RadnikController.php
@@ -5,8 +5,21 @@ class RadnikController extends AdminController
     private $viewDir = 'privatno' . DIRECTORY_SEPARATOR . 'radnik' . DIRECTORY_SEPARATOR;
     public function index()
     {
+        $radnici= Radnik::ucitajSve();
+        foreach($radnici as $radnik){
+             if(strlen($radnik->lozinka)>20){
+                $radnik->lozinka= substr($radnik->lozinka,0,20) . '...';
+            }
+            if(strlen($radnik->komentar)===0){
+                $radnik->komentar= 'Nema komentar';
+            }
+             if(strlen($radnik->komentar)>20){
+                $radnik->komentar=substr($radnik->komentar,0,20) . '...';
+            }
+
+        }
         $this->view->render($this->viewDir . 'index',[
-            'radnici'=>Radnik::ucitajSve()
+            'radnici'=>$radnici
         ]);
         
     }
