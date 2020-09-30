196730c7b5ae502cc495a9672b83805eac21a3ff
Ivan Ivansic <iivansic@samedeutz-fahr.hr>
2020-09-30 13:06:55 +0200

    update skripte za popunu baze

--- Script-1.sql
+++ Script-1.sql
@@ -5,6 +5,10 @@ inner join glavnatablica b on a.glavnatablica=b.id;
 select * from glavnatablica;
 select * from kataforeza;
 select * from povijestkretanjanaloga;
+insert into povijestkretanjanaloga (glavnatablica, radnik, kolicina, status, lokacija, stroj, opis) values
+(2,2,15,4,'kraj regala','Laser 3', 'Nema škarta'),
+(3,3,22,4,'kraj regala','Savijačica 2', 'Nema škarta'),
+(4,4,36,5,'kraj regala','Savijačica 5', 'Nema škarta');
 
 insert into kataforeza (prioritet, glavnatablica , lokacija, stanje, stiglo, otislo) values
 ('1','1','B12',50,50,0),
@@ -16,6 +20,7 @@ select id from glavnatablica where partnumber ='16063115';
 
 select b.partnumber from kataforeza a inner join glavnatablica b on a.glavnatablica=b.id where a.id=1;
 select b.partnumber from kataforeza a inner join glavnatablica b on a.glavnatablica=b.id where a.id=b.id;
+#ovo ispod radi al ne mogu ubacit to isto u php :S 
 update kataforeza set stanje = stanje - 5, minobojat = minobojat - 5, otislo = otislo + 5 where id=1;
 
 select b.partnumber, concat(c.ime, ' ', c.prezime), a.kolicina, d.naziv, a.lokacija, a.stroj, a.opis, a.datum 
