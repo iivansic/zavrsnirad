2c19fbf6454d2d587f50728a3a2dcb17da8a1be0
iivansic <ivan.ivansic@sdfgroup.com>
2020-10-06 21:31:24 +0200

    sad i status pretrazuje na glavnoj tablici dodan autofocus u input box

--- model/Glavnatablica.php
+++ model/Glavnatablica.php
@@ -6,8 +6,11 @@ class Glavnatablica
         $od = $stranica * 11 - 11;
         $veza = DB::getInstanca();
         $izraz = $veza->prepare('
-        select * from glavnatablica where concat(partnumber, \' \', naziv, \' \', ifnull(lokacija, \' \'), ifnull(status, \' \'), ifnull(takt, \' \')) like :uvjet limit :od, 11;
-            ');
+        select a.id, a.partnumber, a.naziv, a.stanje, a.lokacija, a.status, a.tehnologija, a.takt, a.opis from glavnatablica a 
+        left join status b on a.status=b.id
+        where concat(a.partnumber, \' \', ifnull(a.naziv, \' \'), ifnull(a.lokacija, \' \'), 
+        ifnull(b.naziv, \' \'), ifnull(a.takt, \' \'), ifnull(a.opis, \' \')) like :uvjet limit :od, 11;
+        ');
         $izraz->bindParam('uvjet',$uvjet);
         $izraz->bindValue('od',$od,PDO::PARAM_INT);
         $izraz ->execute();
